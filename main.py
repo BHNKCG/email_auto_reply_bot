863b610fab2f00b43d8269fe676c15c6311853ff
Kushan Chamod <kushanchamod2001@gmail.com>
2026-01-14 18:16:06 +0530

    Add basic email keyword logic

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Automated Email Reply Bot Started successfully!")
+def check_email(text):
+    if "price" in text:
+        return "Send price reply"
+    else:
+        return "Send default reply"
+    
+email_text = "Hello, I need your price detail"
+result = check_email(email_text)
+print(result)
